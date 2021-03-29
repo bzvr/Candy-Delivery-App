5075df91c8b3effe10b23d08d5662332d2b92281
leonid <lbezvershenkoo@yandex.ru>
2021-03-29 16:43:01 +0300

    Add allowed_hosts to env

--- server/server/settings.py
+++ server/server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", 'prikol')
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split()
 
 
 # Application definition
