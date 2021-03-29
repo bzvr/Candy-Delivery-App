bb5930e5bdd331d37978af3ab54e0e4d3be30684
leonid <lbezvershenkoo@yandex.ru>
2021-03-29 16:25:07 +0300

    Edit url patterns

--- server/candy_delivery_app/urls.py
+++ server/candy_delivery_app/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from candy_delivery_app import views
 
 urlpatterns = [
-    re_path('couriers/?$', views.CourierView.as_view()),
+    path('couriers', views.CourierView.as_view()),
     path('couriers/<int:pk>', views.CourierView.as_view()),
-    re_path('orders/?$', views.OrderView.as_view()),
-    re_path('orders/assign/?$', views.AssignOrderView.as_view()),
-    re_path('orders/complete/?$', views.CompleteOrderView.as_view()),
+    path('orders', views.OrderView.as_view()),
+    path('orders/assign', views.AssignOrderView.as_view()),
+    path('orders/complete', views.CompleteOrderView.as_view()),
 ]
 
